8502e0115ac1f885067766c802ea95b07b962517
JIOjosBG <jjjooosssiiifff@gmail.com>
2023-04-02 11:59:04 +0300

    new

--- engen/src/components/index.tsx
+++ engen/src/components/index.tsx
@@ -4,5 +4,4 @@ import Spinner from "./Spinner";
 import CreateGameComponent from "./CreateGame/CreateGameComponent";
 import ListComponent from './ListComponent/ListComponent';
 
-
 export { LoginComponent, LoginButton, Spinner, CreateGameComponent, ListComponent };
